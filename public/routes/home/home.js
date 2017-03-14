8cdd57473567efdf5bf43a144a59eb1c825bcc37
Seth Sutherland <cokolatepal@gmail.com>
2017-03-14 17:44:02 -0600

    Got the main title on the home page working, fluid effect integrated on home page.  Small adjustments on the nav icon.

--- public/routes/home/home.js
+++ public/routes/home/home.js
@@ -1,4 +1,13 @@
 angular.module("topSecret")
-.controller("homeController", function($scope, $stateParams){
-  $scope.test = 'home'
-})
+.controller("homeController", function($scope, $stateParams, $window) {
+
+    function fluidInit(){
+        $window.FluidEffect.init();
+        $window.FluidEffect.start();
+    }
+
+    $window.onload(fluidInit());
+
+
+
+});
