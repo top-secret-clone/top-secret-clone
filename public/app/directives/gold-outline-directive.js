45b0cdebac477754f46d4052ebe2ca9296d714a6
Seth Sutherland <cokolatepal@gmail.com>
2017-03-16 09:52:05 -0600

    Gold outline animation working

--- public/app/directives/gold-outline-directive.js
+++ public/app/directives/gold-outline-directive.js
@@ -6,24 +6,29 @@ angular.module('topSecret').directive('goldOutline', function(){
 
     return {
         restrict: 'E',
-        template: '<div class="gold-outline-container" style="z-index: 10; position: absolute; height: 40vh; width: 28vw">' +
-            '<div id="gold-top-right" style="margin-top: 10px; margin-left: 14vw; width: 0; height: 2px; background-color: #88714d"></div>' +
-            '<div id="gold-right" style="width: 2px; height: 0; background-color: #88714d; margin-left: 507px; margin-top: -2px"></div>' +
-            '<div id="gold-bottom" style="width: 0; height: 2px; background-color: #88714d; margin-top: -2px; margin-left: 12px"></div>' +
-            '<div id="gold-left" style="width: 2px; height: 0; background-color: #88714d; margin-top: -365px; margin-left: 11px"></div>' +
-            '<div id="gold-top-left" style="width: 0; height: 2px; background-color: #88714d; margin-left: 12px; margin-top: -365px"></div>' +
-         '</div>',
-        link: function(scope, element, attributes){
-
-            // scope.worm = function(){
-            //     console.log('yo');
-            //     // TweenMax.to($('.gold-top-right'), 0.5, {width: '249px'});
-            //     // TweenMax.to($('.gold-right'), 0.5, {delay: '0.5',height: '365px'});
-            //     // TweenMax.to($('.gold-bottom', 0.5, {delay: '1', width: '497px'}));
-            //     // TweenMax.to($('.gold-left'), 0.5, {delay: '1.5', height: '365px'});
-            //     // TweenMax.to($('.gold-top-left'), 0.5, {delay: '2', width: '255px'});
-            // };
-
+        templateUrl: './app/directives/gold-outline.html',
+        controller: function($scope){
+            $scope.worm = function () {
+                console.log('the worm....');
+                TweenMax.to(document.getElementById('gold-top-right'), 0.5, {width: '249px'});
+                        TweenMax.to(document.getElementById('gold-right'), 0.5, {delay: '0.5',height: '365px'});
+                        TweenMax.to(document.getElementById('gold-bottom'), 0.5, {delay: '1', width: '497px'});
+                        TweenMax.to(document.getElementById('gold-left'), 0.5, {delay: '1.5', height: '365px'});
+                        TweenMax.to(document.getElementById('gold-top-left'), 0.5, {delay: '2', width: '255px'});
+            }
         }
+        // link: function(scope, element, attributes){
+        //
+        //
+        //     element.on('mouseover', function(){
+        //         console.log('yo');
+        //         TweenMax.to($('.gold-top-right'), 0.5, {width: '249px'});
+        //         TweenMax.to($('.gold-right'), 0.5, {delay: '0.5',height: '365px'});
+        //         TweenMax.to($('.gold-bottom', 0.5, {delay: '1', width: '497px'}));
+        //         TweenMax.to($('.gold-left'), 0.5, {delay: '1.5', height: '365px'});
+        //         TweenMax.to($('.gold-top-left'), 0.5, {delay: '2', width: '255px'});
+        //     });
+        //
+        // }
     }
 });
