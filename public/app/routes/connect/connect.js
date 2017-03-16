e673788a2dbc81b5d4bf165f5453f87c3904fd52
Khayyam Jones <Khayyam.Jones@gmail.com>
2017-03-15 19:33:25 -0600

    adjusted styling and font of page; still requires positioning and work on the form

--- public/app/routes/connect/connect.js
+++ public/app/routes/connect/connect.js
@@ -1,4 +1,8 @@
 angular.module("topSecret")
 .controller("connectController", function($scope, $stateParams){
   $scope.test = 'connect'
+
+
+  const height = $(window).height();
+  console.log('win height ', height)
 })
