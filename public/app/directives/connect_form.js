8660c2dee2555cd0325707c1164b81ec5757aa94
Seth Sutherland <cokolatepal@gmail.com>
2017-03-22 11:23:33 -0600

    why??

--- public/app/directives/connect_form.js
+++ public/app/directives/connect_form.js
@@ -9,16 +9,18 @@ angular.module('topSecret')
     // link: function (scope, elem, attrs) {
     //
     // }, /* end of Link function */
-    controller: function($http) {
-      this.submitForm = function(connect) {
-        return http.get({
-          method: 'POST',
-          url: '',
-          data: connect
-        }).then( function(response) {
-          return response.data;
-        })
+    controller: function($scope,$http) {
+
+      $scope.submitForm = function(connect) {
+        console.log('form submitted...');
+        // return http.get({
+        //   method: 'POST',
+        //   url: '',
+        //   data: connect
+        // }).then( function(response) {
+        //   return response.data;
+        // })
       }
-    }
+    } /* end of controller */
   } /* end of return object */
 })
