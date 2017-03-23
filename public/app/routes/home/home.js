cabc3d9691828c7347c3e51727f6313f5af03521
Seth Sutherland <cokolatepal@gmail.com>
2017-03-23 15:24:25 -0600

    the parallax on the home page is working

--- public/app/routes/home/home.js
+++ public/app/routes/home/home.js
@@ -1,16 +1,29 @@
 angular.module("topSecret")
-.controller("homeController", function($scope, $stateParams, $window) {
+.controller("homeController", function($scope, $stateParams, $window, $timeout) {
 
-   $scope.maintainOutline = function(){
+    let homeCasePara = document.getElementById('parallax-holder-thingy');
+    let homeWorkPara = document.getElementById('work');
+    let homeCapPara = document.getElementById('capabilities');
+    let homeConPara = document.getElementById('connect');
+
+   $window.onscroll = function(){
+
+       let paraAmount = window.pageYOffset;
+       // console.log(paraAmount);
+
+       let paraStart = paraAmount * 0.20 - 200;
+
+       homeCasePara.style.top = paraStart + 'px';
 
    };
-    // function fluidInit(){
-    //     window.FluidEffect.init();
-    //     window.FluidEffect.start();
-    // }
-    //
-    // $window.onload(fluidInit());
 
 
+    //you HAVE to have the fluid initializer in a timeout,
+    //otherwise the gold-outline directive doesn't work
+    $timeout(
+        function fluidInit(){
+        $window.FluidEffect.init();
+        $window.FluidEffect.start();
+    }, 0);
 
 });
