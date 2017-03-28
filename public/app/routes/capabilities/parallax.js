80ad3dc57825b5547cbe6a9c5104bc6efaa77b73
Khayyam Jones <Khayyam.Jones@gmail.com>
2017-03-27 22:22:17 -0600

    continuing adjustments for media queries (stopping point for night)

--- public/app/routes/capabilities/parallax.js
+++ public/app/routes/capabilities/parallax.js
@@ -10,14 +10,11 @@ angular.module("topSecret")
     link: function (scope, elem, attrs) {
       const win = $(window);
       const height = win.height(); /* calculate actual window height */
+      const winWidth = win.width();
       const adjustedViewHeight = height < 775 ? 775 - height : 0;
       const start = parseInt(scope.start);
       const startPosition = height < 775 ? start - (775 - height): start;
       const section = `.${scope.foo}`;
-      $(section).css('top', startPosition);
-
-
-
       const elementTable = {
         element: scope.foo,
         window: height,
@@ -26,66 +23,59 @@ angular.module("topSecret")
         animationBegin: parseInt(scope.start),
         animationEnd: parseInt(scope.start) + 450
       }
-      // console.log(elementTable);
-      // console.log(section,' start: ', start);
+
+      if (winWidth > 750) { /* size of screen when media queries begin */
+        console.log('adjusted start for full-screen effect');
+        $(section).css('top', startPosition);
+      }
 
       setTimeout(function() {
-        $(window).scroll( () => {
-          var scrollTop = pageYOffset;
-          // var adjustedViewHeight = height < 775 ? 775 - height : 0;
-          var startAnimation = start + adjustedViewHeight;
-          var endAnimation = startAnimation + 450;
-          var scrollCounter = scrollTop - startAnimation;
-          var entryOpacity = scrollCounter > 0 && scrollCounter <= 100 ? scrollCounter / 100 : 1;
-          var exitOpacity = scrollCounter > 450 && scrollCounter <=500 ? (500 - scrollCounter) / 100 : 0;
-          var parallaxAdjust = scrollCounter > 0 && scrollCounter < 550 ? ((scrollCounter / 1100) + 0.75) * -1 : -1;
-          scope.parallaxScroll = (scrollTop - startAnimation) * parallaxAdjust;
-          // console.log('parallaxScroll',scope.parallaxScroll);
-          // console.log(section, '-> scroll: ',scrollTop, 'startAnimation: ', startAnimation, 'endAnimation: ', endAnimation, 'viewPort Count: ', scrollCounter, 'Parallax Adjust: ', parallaxAdjust);
+          $(window).scroll( () => {
+            if (winWidth > 750) { /* if width < 750 don't run parallax function */
+              console.log("winWidth > 750px");
 
-          /* while item is on screen */
-          if (scrollTop > startAnimation && scrollTop <= endAnimation) {
-          var translate = 'translateY('+scope.parallaxScroll+'px)'
-          $(section).addClass('test');
-          $(section).css('transform', translate);
-          $(section).css('opacity', entryOpacity);
-          }
+              var scrollTop = pageYOffset;
+              var startAnimation = start + adjustedViewHeight;
+              var endAnimation = startAnimation + 450;
+              var scrollCounter = scrollTop - startAnimation;
+              var entryOpacity = scrollCounter > 0 && scrollCounter <= 100 ? scrollCounter / 100 : 1;
+              var exitOpacity = scrollCounter > 450 && scrollCounter <=500 ? (500 - scrollCounter) / 100 : 0;
+              var parallaxAdjust = scrollCounter > 0 && scrollCounter < 550 ? ((scrollCounter / 1100) + 0.75) * -1 : -1;
+
+              scope.parallaxScroll = (scrollTop - startAnimation) * parallaxAdjust;
+
+              /* while item is on screen */
+              if (scrollTop > startAnimation && scrollTop <= endAnimation) {
+                console.log('engaging parallax-->', section)
+                var translate = 'translateY('+scope.parallaxScroll+'px)'
+                $(section).addClass('test');
+                $(section).css('transform', translate);
+                $(section).css('opacity', entryOpacity);
+              } /* end of if (scrollTop...) */
 
 
-   /*
-    * before item gets on screen
-    * scrollTop (how far down the screen)
-    * start = how far to get to bottom of Viewport
-    * scrollTop - start = 0 is bottom of screen
-    * plus adjustedViewHeight to display on the screen
-    * before animations begin
-    * var startAnimation = start + adjustedViewHeight;
-    */
-          else if (scrollTop < startAnimation){
-            $(section).removeClass('test');
-          }
+       /*
+        * before item gets on screen
+        */
+              else if (scrollTop < startAnimation){
+                $(section).removeClass('test');
+              } /* end else if (scrollTop...) */
 
 
 
-   /*
-    * after item leaves screen
-    * scrollTop (how far down the screen)
-    * start = how far to get to the bottom of the screen
-    * height = distance to top of screen
-    * scrollTop = start + height is when the top of the
-    *   div reaches the top of the screen
-    * scrollTop = start + height + 600 (height of div) is
-    *   when the bottom of the screen reaches the top
-    * var endAnimation = startAnimation + height + 450;
-    *   450 because -50 from startAnimation and end with
-    *   100px remaining on screen
-    */
-          else if (scrollTop > endAnimation){
-            $(section).removeClass('test');
-            $(section).css('opacity', exitOpacity);
-          }
-        });
-      },0) /* end timeout function */
+       /*
+        * after item leaves screen
+        */
+              else if (scrollTop > endAnimation){
+                $(section).removeClass('test');
+                $(section).css('opacity', exitOpacity);
+              } /* end else if (scrollTop...) */
+
+            }
+          }); /* end window scroll function */
+
+      },0); /* end timeout function */
+
     }  /* end Link function */
   }
 })
